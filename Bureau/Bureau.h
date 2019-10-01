8b83a8f30b405bb9ae43041596c4bf5c9f3b19a3
Muukii <muukii.app@gmail.com>
2019-10-01 20:51:02 +0900

    Initial Commit

---
+++ Bureau/Bureau.h
@@ -0,0 +1,19 @@
+//
+//  Bureau.h
+//  Bureau
+//
+//  Created by muukii on 2019/10/01.
+//  Copyright © 2019 muukii. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Bureau.
+FOUNDATION_EXPORT double BureauVersionNumber;
+
+//! Project version string for Bureau.
+FOUNDATION_EXPORT const unsigned char BureauVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bureau/PublicHeader.h>
+
+
